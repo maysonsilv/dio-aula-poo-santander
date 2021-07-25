cbf271a452e1706714aa7f1adc2db7d104f66849
maysonsilv <mayson.sillva@gmail.com>
2021-07-24 21:49:08 -0300

    created enum client type and test to it

---
+++ src/main/kotlin/one/digitalinnovation/diobank/ClienteTipo.kt
@@ -0,0 +1,6 @@
+package one.digitalinnovation.diobank
+
+enum class ClienteTipo(val descricao: String){
+    PF("Pessoa Física"),
+    PJ( "Pessoa Jurídica")
+}
---
+++ src/main/kotlin/one/digitalinnovation/diobank/TesteClienteTipoEnum.kt
@@ -0,0 +1,18 @@
+package one.digitalinnovation.diobank
+
+import one.digitalinnovation.diobank.ClienteTipo
+fun main(){
+    ClienteTipo.values().forEach {elemento ->
+        println("${elemento.name} - ${elemento.descricao}")
+    }
+
+    ClienteTipo.values().forEach {
+        println("${it.name} - ${it.descricao}")
+    }
+
+    val pf = ClienteTipo.PF
+    println(">> ${pf.name} - ${pf.descricao}")
+
+    val pj = ClienteTipo.PJ
+    println(">> ${pj.name} - ${pj.descricao}")
+}
